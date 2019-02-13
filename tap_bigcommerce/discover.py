efd9e0f32fdb38167eb5a406ce6fff65742baff3
Chris Goddard <chris@chrisgoddard.me>
2019-02-12 16:21:00 -0800

    Update discover.py

--- tap_bigcommerce/discover.py
+++ tap_bigcommerce/discover.py
@@ -1,21 +1,3 @@
-"""
-Discovery Mode: describe data streams available and creates a
-catalog.json object
-
-https://github.com/singer-io/getting-started/blob/master/docs/DISCOVERY_MODE.md
-
-Schemas can either be hardcoded or defined from an API.
-
-TODO:
-
-1)  Develop generic template for discover mode that can work with
-    either hard-coded schema files or a API-defined
-
-2)  Command-line utility for customizing catalog.json based on stream
-    field selection / inclusion
-
-
-"""
 
 import os
 import singer
@@ -25,7 +7,6 @@ from tap_bigcommerce.streams import STREAMS
 
 def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
-
 
 def discover_streams(client):
     streams = []
