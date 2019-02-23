7128534df7d127a1f785569e5f19e991e049ee4d
Chris Goddard <chris@chrisgoddard.me>
2019-02-22 16:27:21 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_bigcommerce"],
     install_requires=[
-        "singer-python==5.0.12",
+        "singer-python==5.4.1",
         "requests==2.21.0",
         "requests-futures==0.9.9"
     ],
