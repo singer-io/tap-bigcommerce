1d9d63992b4680e22f9473a887d76d789126692a
akkumar-qlik <akarsh.kumar@qlik.com>
2026-05-11 16:28:50 +0530

    Upgraded the library

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     py_modules=["tap_bigcommerce"],
     install_requires=[
         "singer-python==6.8.0",
-        "requests==2.32.5",
+        "requests==2.33.1",
         "requests-futures==1.0.2"
     ],
     extras_require={
